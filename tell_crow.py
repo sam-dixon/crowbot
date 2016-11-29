b138def60392796d6bfe5901795c97b0c6060996
Samantha Dixon <sam.dixon@berkeley.edu>
2016-11-28 21:22:33 -0800

    Move channel id to key file

--- tell_crow.py
+++ tell_crow.py
@@ -1,9 +1,9 @@
 import sys
 from slackclient import SlackClient
-from keys import BOT_ID, CROWBOT_API
+from keys import CROWBOT_API, GROUP_CHANNEL_ID
 
 SC = SlackClient(CROWBOT_API)
-CHANNEL = 'C0VHU6ES3'
+CHANNEL = GROUP_CHANNEL_ID
 message = sys.argv[1]
 SC.api_call('chat.postMessage', as_user=True,
-                channel=CHANNEL, text=message)
+            channel=CHANNEL, text=message)

9cdd8685f192ebe21c1226de9ea19d6c3e853407
Samantha Dixon <sam.dixon@berkeley.edu>
2016-12-06 17:32:44 -0800

    Fix configuration in tell crow

--- tell_crow.py
+++ tell_crow.py
@@ -1,11 +1,12 @@
 """Command line script to have crow send messages to the group channel"""
 
 import sys
+import yaml
 from slackclient import SlackClient
-from keys import CROWBOT_API, GROUP_CHANNEL_ID
 
-SC = SlackClient(CROWBOT_API)
-CHANNEL = GROUP_CHANNEL_ID
+CONFIG = yaml.load(open(CONFIG.yml, 'r'))
+SC = SlackClient(CONFIG['slack_info']['crowbot_api'])
+CHANNEL = CONFIG['slack_info']['group_channel_id']
 message = str(sys.argv[1])
 SC.api_call('chat.postMessage', as_user=True,
             channel=CHANNEL, text=message)
